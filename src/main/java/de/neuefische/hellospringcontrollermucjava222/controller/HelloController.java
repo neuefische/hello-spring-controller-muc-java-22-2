e587a7b16d83de088e13042f7604b66ed2487ecc
Sergej Grilborzer <sergej@grilborzer.de>
2022-11-07 15:59:14 +0100

    Update controller with post and pathvariable

--- src/main/java/de/neuefische/hellospringcontrollermucjava222/controller/HelloController.java
+++ src/main/java/de/neuefische/hellospringcontrollermucjava222/controller/HelloController.java
@@ -1,2 +1,27 @@
-package de.neuefische.hellospringcontrollermucjava222.controller;public class HelloController {
+package de.neuefische.hellospringcontrollermucjava222.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greeting")
+public class HelloController {
+
+    // Daten anfordern
+    @GetMapping
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/{lieblingsCurrywurstBudde}")
+    public String sayHello(@PathVariable String lieblingsCurrywurstBudde) {
+        return "Deine geliebteste Currywurstbudde lautet: " + lieblingsCurrywurstBudde;
+    }
+
+    // Daten verschicken
+    @PostMapping
+    public String acceptHello(@RequestBody String request) {
+        return "Hi, deine Anfrage war: " + request;
+    }
 }
