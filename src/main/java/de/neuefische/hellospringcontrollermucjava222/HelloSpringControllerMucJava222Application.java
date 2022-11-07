5f3cd9ca10bc2ea6f330d816817ad63d74435dfc
Sergej Grilborzer <sergej@grilborzer.de>
2022-11-07 12:13:46 +0100

    Create HelloWorldController

--- src/main/java/de/neuefische/hellospringcontrollermucjava222/HelloSpringControllerMucJava222Application.java
+++ src/main/java/de/neuefische/hellospringcontrollermucjava222/HelloSpringControllerMucJava222Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloSpringControllerMucJava222Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringControllerMucJava222Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringControllerMucJava222Application.class, args);
+    }
 
 }
--- src/main/java/de/neuefische/hellospringcontrollermucjava222/controller/HelloController.java
+++ src/main/java/de/neuefische/hellospringcontrollermucjava222/controller/HelloController.java
@@ -1,2 +1,29 @@
-package de.neuefische.hellospringcontrollermucjava222.controller;public class HelloController {
+package de.neuefische.hellospringcontrollermucjava222.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greeting")
+public class HelloController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hi there, hello!";
+    }
+
+    @GetMapping("/{name}")
+    public String sayHelloTo(@PathVariable String name) {
+        return "Hi there, hello " + name + "!";
+    }
+
+    @GetMapping("/{name}/and")
+    public String sayHelloTo(@PathVariable String name, @RequestParam String name2) {
+        return "Hi there, hello " + name + " and " + name2 + "!";
+    }
+
+    @GetMapping("/{name}/{name2}")
+    public String sayHelloToAndTo(@PathVariable String name, @PathVariable String name2) {
+        return "Hi there, hello " + name + " and " + name2 + "!";
+    }
+
 }
